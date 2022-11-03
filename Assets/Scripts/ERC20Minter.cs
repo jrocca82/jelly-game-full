be596cf4886055ee9f1c7aeb3775c6ea6b9cec1c
Jo <loluvulol@gmail.com>
2022-11-03 13:40:45 +1000

    Add erc20

---
+++ Assets/Scripts/ERC20Minter.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NOTE: For chainsafe SDK, all strings need a backslash before quotes
+// in ABI and args (i.e. "[\"10\"])
+public class ERC20Minter : MonoBehaviour
+{
+    // Set chain -- use for EVM.Call() to call method w/o connecting user wallet
+    // string chain = "ethereum";
+    // string network = "goerli";
+
+    // Set contract
+    private string
+        contractAddress = "0x1f6f34E708A154F7512aDdFAD7e24812b86a4579";
+
+    private readonly string
+        abi =
+            "[{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"Approval\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"previousOwner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"OwnershipTransferred\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"event\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"}],\"name\":\"allowance\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"approve\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"account\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"burn\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"account\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"burnFrom\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"decimals\",\"outputs\":[{\"internalType\":\"uint8\",\"name\":\"\",\"type\":\"uint8\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"subtractedValue\",\"type\":\"uint256\"}],\"name\":\"decreaseAllowance\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"addedValue\",\"type\":\"uint256\"}],\"name\":\"increaseAllowance\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"mint\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"renounceOwnership\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"symbol\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"transferFrom\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"transferOwnership\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]";
+
+    public async void MintToken()
+    {
+        Debug.Log("MINTING");
+        // // Method name
+        string method = "mint";
+
+        // Replace placeholders with actual values
+        string args = "[\"accountAddress\", \"tokensEarned\"]";
+
+        // Add 0s to convert value to uint256
+        string tokensEarned = (TextManager.instance.score / 100).ToString() + "000000000000000000";
+        string userAddress = PlayerPrefs.GetString("Account");
+        string newArgs = args.Replace("tokensEarned", tokensEarned);
+        string argsFull = newArgs.Replace("accountAddress", userAddress);
+        Debug.Log(argsFull);
+
+        // Value in wei
+        string value = "0";
+
+        // Gas
+        string gasLimit = "";
+        string gasPrice = "";
+
+        // Mint function
+        try
+        {
+            // Connect to user wallet and mint tokens
+            string resp =
+                await Web3GL
+                    .SendContract(method,
+                    abi,
+                    contractAddress,
+                    argsFull,
+                    value,
+                    gasLimit,
+                    gasPrice);
+            Debug.Log (resp);
+            TextManager.instance.ResetScore();
+        }
+        catch (Exception e)
+        {
+            Debug.Log (e);
+        }
+    }
+}
--- Assets/Scripts/TextManager.cs
+++ Assets/Scripts/TextManager.cs
@@ -1,33 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextManager : MonoBehaviour
 {
-    public TextMeshProUGUI textMesh;
+    public TextMeshProUGUI scoreMesh;
+
+    public TextMeshProUGUI userAddress;
+
+    public TextMeshProUGUI tokenBalance;
 
     public static TextManager instance;
 
-    int score;
+    public GameObject coinMintButton;
+
+    public int score;
 
     void Start()
     {
+        // Display wallet address
+        string userAddressText = PlayerPrefs.GetString("Account");
+        string startAddress = userAddressText.Substring(0, 5);
+        string endAddress = userAddressText.Substring(userAddressText.Length - 5, 5);
+        string shortenedAddress = startAddress + "..." + endAddress;
+        userAddress.text = "Connected to: " + shortenedAddress;
         if (instance == null)
         {
             instance = this;
         }
         score = 0;
-        textMesh.text = "Coins: " + score;
+        scoreMesh.text = "Coins: " + score;
     }
 
     void Update()
     {
+        DisplayERC20Balance();
+        if (score < 100)
+        {
+            coinMintButton.SetActive(false);
+        }
+        else
+        {
+            coinMintButton.SetActive(true);
+        }
     }
 
     public void IncreaseScore()
     {
         score++;
-        instance.textMesh.text = "Coins: " + score;
+        instance.scoreMesh.text = "Coins: " + score;
+    }
+
+    public void ResetScore()
+    {
+        score = score % 100;
+        instance.scoreMesh.text = "Coins: " + score;
+    }
+
+    public async void DisplayERC20Balance()
+    {
+        //Display token balances
+        string chain = "ethereum";
+        string network = "goerli";
+        string contract = "0x1f6f34E708A154F7512aDdFAD7e24812b86a4579";
+        string account = PlayerPrefs.GetString("Account");
+        BigInteger balanceOf = await ERC20.BalanceOf(chain, network, contract, account);
+
+        BigInteger weiValue = 1000000000000000000;
+        int balance = (int)(balanceOf / weiValue);
+        tokenBalance.text = "Gold Token Balance: " + balance.ToString();
     }
 }
